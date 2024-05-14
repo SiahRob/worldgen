9134f4a614825c79408cf9248436c5a3dc193067
SiahRob <168476914+SiahRob@users.noreply.github.com>
2024-05-14 12:24:56 -0500

    comments

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ window = tsapp.GraphicsWindow(1670, 682, (200, 200, 255))
 
 # need to add a camera instead of chunk based.
 # gotta rework how it gets compiled into chunks, rather give all blocks their unique position
-#  add two variables, x and y position, as the player moves, x and y position are updated
+# add two variables, x and y position, as the player moves, x and y position are updated
 # camera is centered to the players position
 # add an area around the camera, anything within that area is rendered, make it bigger than the screen ofc
-# 
+# have a list of all the blocks that are rendered, since everything has its own unique value, there is no need for a list matrix
+# check for collision for the blocks that are in that list, going to have to rework main even more :/ 
 
 
 def load_chunks(chunkrem, chunkadd):
