081468843df66679ab3ffa65f0635efa63f3e8db
SiahRob <168476914+SiahRob@users.noreply.github.com>
2024-05-16 12:28:36 -0500

    COLLISIONN!!!!!

--- main w camera.py
+++ main w camera.py
@@ -23,26 +23,42 @@ player.scale = 0.5
 player.center_y = window.height / 2
 player.center_x = window.width / 2
 window.add_object(player)
-
+rendered_blocks = []
+block_coll_var = 56
 while window.is_running:
     mouse_x, mouse_y = tsapp.get_mouse_position()
 
     for i in range(len(world)):
         if -100 < world[i].sprite.center_x < window.width + 100 and -100 < world[i].sprite.center_y < window.height + 100:
             window.add_object(world[i].sprite)
+            rendered_blocks.append(world[i])
         if -100 < world[i].sprite.center_x < window.width + 100 and -100 < world[i].sprite.center_y < window.height + 100:
             world[i].sprite.destroy()
+            rendered_blocks.remove(world[i])
         if world[i].sprite.is_colliding_point((mouse_x, mouse_y)) and tsapp.was_mouse_pressed():
-            air = tsapp.Sprite("sprites/air block.png", 0, 0)
+            air = tsapp.Sprite("sprites/air block.png", world[i].sprite.x, world[i].sprite.y)
             block = blocks.Block(air, (), "air block", False)
+            window.add_object(air)
             world[i] = block
 
     if tsapp.is_key_down(tsapp.K_d):
         for i in range(len(world)):
-            world[i].sprite.x -= 20
+            block_right_face_coll = world[i].sprite.center_x - block_coll_var <= player.x + player.width <= world[i].sprite.center_x + block_coll_var and (player.y + player.height) > world[i].sprite.y + 15 and player.y < world[i].sprite.y + world[i].sprite.width
+            if block_right_face_coll and world[i].block_has_collision:
+                break
+        else:
+            for i in range(len(world)):
+                world[i].sprite.x -= 10
+
     elif tsapp.is_key_down(tsapp.K_a):
         for i in range(len(world)):
-            world[i].sprite.x += 20
+            block_left_face_coll = world[i].sprite.center_x - block_coll_var <= player.x <= world[i].sprite.center_x + block_coll_var and (player.y + player.height) > world[i].sprite.y + 15 and player.y < world[i].sprite.y + world[i].sprite.width
+            if block_left_face_coll and world[i].block_has_collision:
+                break
+        else:
+            for i in range(len(world)):
+                world[i].sprite.x += 10
+
     if tsapp.is_key_down(tsapp.K_w):
         for i in range(len(world)):
             world[i].sprite.y += 20
