87be6dfa0c68762328aa6495e62c51478b9204d1
SiahRob <168476914+SiahRob@users.noreply.github.com>
2024-05-17 12:28:16 -0500

    somewhat fixed collision and added gravity and you can fly

--- main w camera.py
+++ main w camera.py
@@ -8,15 +8,11 @@ pygame.init()
 world_class = w.WorldGeneration(1)
 
 world = world_class.generate_world()
-window = tsapp.GraphicsWindow(1070, 682, (200, 200, 255))
+window = tsapp.GraphicsWindow(1920, 1020, (200, 200, 255))
 
-# need to add a camera instead of chunk based.
-# gotta rework how it gets compiled into chunks, rather give all blocks their unique position
-# add two variables, x and y position, as the player moves, x and y position are updated
-# camera is centered to the players position
-# add an area around the camera, anything within that area is rendered, make it bigger than the screen ofc
-# have a list of all the blocks that are rendered, since everything has its own unique value, there is no need for a list matrix
-# check for collision for the blocks that are in that list, going to have to rework main even more :/
+# got camera working, need to figure out how to get smooth collision.
+# ideas to spruce up the generation a bit.
+# Iron ore, Coal Ore, Gold ore, diamond ore, trees
 
 player = tsapp.Sprite("sprites/player person.png", 0, 0)
 player.scale = 0.5
@@ -42,54 +38,57 @@ while window.is_running:
             world[i] = block
 
     if tsapp.is_key_down(tsapp.K_d):
-        """
+
         for i in range(len(world)):
             block_right_face_coll = world[i].sprite.center_x - block_coll_var <= player.x + player.width <= world[i].sprite.center_x + block_coll_var and (player.y + player.height) > world[i].sprite.y + 15 and player.y < world[i].sprite.y + world[i].sprite.width
             if block_right_face_coll and world[i].block_has_collision:
+                move_var = world[i].sprite.x - (player.x + player.width)
+                for x in range(len(world)):
+                    world[x].sprite.x -= move_var
                 break
         else:
             for i in range(len(world)):
-        """
-        for i in range(len(world)):
-            world[i].sprite.x -= 15
+                world[i].sprite.x -= 8.5
 
     if tsapp.is_key_down(tsapp.K_a):
-        """
         for i in range(len(world)):
             block_left_face_coll = world[i].sprite.center_x - block_coll_var <= player.x <= world[i].sprite.center_x + block_coll_var and (player.y + player.height) > world[i].sprite.y + 15 and player.y < world[i].sprite.y + world[i].sprite.width
             if block_left_face_coll and world[i].block_has_collision:
+                move_var = world[i].sprite.x + world[i].sprite.width - player.x
+                for x in range(len(world)):
+                    world[x].sprite.x -= move_var
                 break
         else:
             for i in range(len(world)):
-        """
-        for i in range(len(world)):
-            world[i].sprite.x += 15
+                world[i].sprite.x += 8.5
 
     if tsapp.is_key_down(tsapp.K_w):
-        """"
-        for i in range(10):
-            for i in range(len(world)):
-                if player.is_colliding_rect(world[i].sprite) and world[i].block_has_collision:
-                    block_bottom_face_coll = world[i].sprite.y + world[i].sprite.width - 56 < player.y < world[i].sprite.y + world[i].sprite.width
-                    if block_bottom_face_coll and world[i].block_has_collision:
-                        break
-            else:
-                for i in range(len(world)):
-        """
-        for i in range(len(world)):
-            world[i].sprite.y += 15
-
-    if tsapp.is_key_down(tsapp.K_s):
-        """
         for i in range(len(world)):
             if player.is_colliding_rect(world[i].sprite) and world[i].block_has_collision:
-                block_top_face_coll = world[i].sprite.y + 56 > player.y + player.height > world[i].sprite.y
-                if block_top_face_coll and world[i].block_has_collision:
+                block_bottom_face_coll = world[i].sprite.y + world[i].sprite.width - 56 < player.y < world[i].sprite.y + world[i].sprite.width
+                if block_bottom_face_coll and world[i].block_has_collision:
+                    move_var = world[i].sprite.y + world[i].sprite.height - (player.y + 33)
+                    print(move_var)
+                    for y in range(len(world)):
+                        world[y].sprite.y -= move_var
                     break
         else:
             for i in range(len(world)):
-        """
+                world[i].sprite.y += 33
+
+    #if tsapp.is_key_down(tsapp.K_s):
+    for i in range(len(world)):
+        if player.is_colliding_rect(world[i].sprite) and world[i].block_has_collision:
+            block_top_face_coll = world[i].sprite.y + 56 > player.y + player.height > world[i].sprite.y
+            if block_top_face_coll and world[i].block_has_collision:
+                move_var = world[i].sprite.y - (player.y + player.height - 3)
+                print(move_var)
+                for y in range(len(world)):
+                    world[y].sprite.y -= move_var
+                break
+    else:
         for i in range(len(world)):
-            world[i].sprite.y -= 15
+            world[i].sprite.y -= 16.5
 
     window.finish_frame()
+
